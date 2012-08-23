1ae244fbabdc029d38239880e12f7d287453a14f
Ben <ben@goinvo.com>
2012-08-23 16:13:04 -0400

    Fixing a few bugs and adding states to the hardware and web

--- Arduino/Trestle/Trestle.cpp
+++ Arduino/Trestle/Trestle.cpp
@@ -1,17 +1,16 @@
 #include "Trestle.h"
 
 int Trestle::registerStation(char* name, char* description, boolean override){
-	// char data[255+strlen(identifier)+strlen(name)+strlen(description)];
-	sprintf(_lastResponse, "name=%s&description=%s&override=%s&identifier=%s", name, description, override ? "true" : "false", _stationIdentifier);
+	strcpy_P(_buffer, (char*)register_string); 
+	sprintf(_lastResponse, _buffer, name, description, override ? "true" : "false", _stationIdentifier);
 	if(makePost("/stations/register.json", _lastResponse)){
 		return evaluateResponse();
 	}
 	return STATUS_POST_ERROR;
 }
 int Trestle::addSensor(char* identifier, char* name, char* description, char* units){
-	// char data[255+strlen(station_identifier) + strlen(identifier)+strlen(name)+strlen(description)+strlen(units)];
-	sprintf(_lastResponse, "name=%s&description=%s&station_identifier=%s&identifier=%s&units=%s", name, description, _stationIdentifier, identifier, units);
-	// sprintf(_lastResponse, "{\"name\":\"%s\",\"description\":\"%s\",\"station_identifier\":\"%s\",\"identifier\":\"%s\",\"units\":\"%s\"}", name, description, _stationIdentifier, identifier, units);
+	strcpy_P(_buffer, (char*)add_sensor_string); 
+	sprintf(_lastResponse, _buffer, name, description, _stationIdentifier, identifier, units);
 
 	if(makePost("/sensor/add.json", _lastResponse)){
 
@@ -20,8 +19,8 @@ int Trestle::addSensor(char* identifier, char* name, char* description, char* un
 	return STATUS_POST_ERROR;
 }
 int Trestle::sendSensorData(char* sensor_identifier, int value, int multiplier){
-	// char data[255+strlen(sensor_identifier)];
-	sprintf(_lastResponse, "station_identifier=%s&sensor_identifier=%s&value=%i&multiplier=%i", _stationIdentifier, sensor_identifier,value, multiplier);
+	strcpy_P(_buffer, (char*)send_sensor_string); 
+	sprintf(_lastResponse, _buffer, _stationIdentifier, sensor_identifier,value, multiplier);
 	if(makePost("/measurement/add.json", _lastResponse)){
 		return evaluateResponse();
 	}
@@ -43,7 +42,8 @@ int Trestle::evaluateResponse(){
 int Trestle::registerAction(char* identifier, char* name, char* description, ActionFunctionPtr func){
 	_biggestAction++;
 	if(_biggestAction < NActions){
-		sprintf(_lastResponse, "station_identifier=%s&identifier=%s&name=%s&description=%s&hardware_id=%i", _stationIdentifier, identifier,name, description, _biggestAction);
+		strcpy_P(_buffer, (char*)register_action_string); 
+		sprintf(_lastResponse, _buffer, _stationIdentifier, identifier,name, description, _biggestAction);
 		if(makePost("/hardware_action/add.json", _lastResponse)){
 			int resp =  evaluateResponse();
 			if(resp == STATUS_SUCCESS){
@@ -54,6 +54,28 @@ int Trestle::registerAction(char* identifier, char* name, char* description, Act
 		return STATUS_POST_ERROR;
 	}
 	return STATUS_OUT_OF_BOUNDS_ERROR;
+}
+
+int Trestle::addState(char* identifier, char* name, char* description){
+	strcpy_P(_buffer, (char*)add_state_string); 
+	sprintf(_lastResponse, _buffer, _stationIdentifier, identifier, name, description);
+
+	if(makePost("/state/add.json", _lastResponse)){
+
+		return evaluateResponse();
+	}
+	return STATUS_POST_ERROR;
+}
+
+int Trestle::setStateValue(char* identifier, char* value){
+	strcpy_P(_buffer, (char*)set_state_string); 
+	sprintf(_lastResponse, _buffer, _stationIdentifier, identifier, value);
+
+	if(makePost("/state/setState.json", _lastResponse)){
+
+		return evaluateResponse();
+	}
+	return STATUS_POST_ERROR;
 }
 
 int Trestle::tick(){
--- Arduino/Trestle/Trestle.h
+++ Arduino/Trestle/Trestle.h
@@ -1,6 +1,14 @@
 #ifndef TRESTLE_H
 #define TRESTLE_H
 #include <Arduino.h>
+#include <avr/pgmspace.h>
+
+	const char register_string[] PROGMEM = "name=%s&description=%s&override=%s&identifier=%s";   // "String 0" etc are strings to store - change to suit.
+	const char add_sensor_string[] PROGMEM = "name=%s&description=%s&station_identifier=%s&identifier=%s&units=%s";
+	const char send_sensor_string[] PROGMEM = "station_identifier=%s&sensor_identifier=%s&value=%i&multiplier=%i";
+	const char register_action_string[] PROGMEM = "station_identifier=%s&identifier=%s&name=%s&description=%s&hardware_id=%i";
+	const char add_state_string[] PROGMEM = "station_identifier=%s&identifier=%s&name=%s&description=%s";
+	const char set_state_string[] PROGMEM = "station_identifier=%s&identifier=%s&current_state=%s";
 
 typedef void (* ActionFunctionPtr) ();
 #define NActions 3
@@ -17,6 +25,8 @@ public:
 	int addSensor(char* identifier, char* name, char* description, char* units);
 	int sendSensorData(char* sensor_identifier, int value, int multiplier);
 	int registerAction(char* identifier, char* name, char* description, ActionFunctionPtr func);
+	int addState(char* identifier, char* name, char* description);
+	int setStateValue(char* identifier, char* value);
 	int tick();
 protected:
 	virtual boolean makePost(char* request, char* data);
@@ -24,8 +34,11 @@ protected:
 	int evaluateResponse();
 	char _lastResponse[255];
 	int _biggestAction;
+	char _buffer[100];
 	char _stationIdentifier[MAX_LENGTH_STATION_IDENTIFIER + 1];
 	ActionFunctionPtr _functions[NActions];
+
+
 
 
 };
